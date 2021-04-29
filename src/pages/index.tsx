2228ee8eb69ad7727e06b140c4bb2f46457b79c6
Zhaojia.qiu <zhaojia.qiu@coolkit.cn>
2021-04-29 10:01:33 +0800

    update 302

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,62 +13,84 @@ import TempCard from '@/components/DeviceCard/TempCard';
 import DualR3Card from '@/components/DeviceCard/DualR3Card';
 import IW100Card from '@/components/DeviceCard/IW100Card';
 import UnsupportedCard from '@/components/DeviceCard/UnsupportedCard';
-import LoginTab from '@/components/LoginTab'
+import LoginTab from '@/components/LoginTab';
 import styles from './index.less';
 
 const { Meta } = Card;
 
-const App: React.FC<{ language: string; getLanguage: Function; deviceList: DeviceInfo[]; saveDeviceList:Function, dispatch: Dispatch; isLogin: boolean; checkUserLogin: Function; }> = ({ isLogin, checkUserLogin, getLanguage, deviceList, saveDeviceList, dispatch}) => {
+const App: React.FC<{
+    language: string;
+    getLanguage: Function;
+    deviceList: DeviceInfo[];
+    saveDeviceList: Function;
+    dispatch: Dispatch;
+    isLogin: boolean;
+    checkUserLogin: Function;
+}> = ({ isLogin, checkUserLogin, getLanguage, deviceList, saveDeviceList, dispatch }) => {
     const [loginTabVisible, setLoginTabVisible] = useState(false);
     const { formatMessage } = useIntl();
 
     useEffect(() => {
-        let code = '';
-        let index = 0;
-        if ((index = window.location.search.indexOf('code=')) !== -1) {
-            code = window.location.search.slice(index+5);
-            getHaToken({
-                code,
-                clientId: window.location.origin
+        let source: EventSource;
+        new Promise((resolve) => {
+            let code = '';
+            let index = 0;
+            if ((index = window.location.search.indexOf('code=')) !== -1) {
+                code = window.location.search.slice(index + 5);
+                getHaToken({
+                    code,
+                    clientId: window.location.origin,
+                }).then(resolve);
+            } else {
+                resolve(0);
+            }
+        }).then(() => {
+            checkUserLogin();
+            getDeviceList({ type: 'init' }).then((res) => saveDeviceList(res.data));
+            getLanguage();
+            source = new EventSource('http://192.168.1.115:3000/api/stream');
+            // Prod
+            // const source = new EventSource('api/stream');
+            source.addEventListener('open', () => {
+                console.log('连接建立成功');
             });
-        }
-
-        checkUserLogin();
-        getDeviceList({ type: 'init' }).then((res) => saveDeviceList(res.data));
-        getLanguage();
-
+            source.addEventListener('message', (e) => {
+                console.log('sse -> deviceList', JSON.parse(e.data));
+                saveDeviceList(JSON.parse(e.data));
+            });
+        });
         // dev
         // const source = new EventSource('http://localhost:3000/api/stream');
-        const source = new EventSource('http://192.168.1.115:3000/api/stream');
-        // Prod
-        // const source = new EventSource('api/stream');
-        source.addEventListener('open', () => {
-            console.log('连接建立成功');
-        });
-        source.addEventListener('message', (e) => {
-            console.log('sse -> deviceList', JSON.parse(e.data));
-            saveDeviceList(JSON.parse(e.data));
-        });
+
         return () => {
-            source.close();
+            if (source) {
+                source.close();
+            }
         };
     }, []);
 
     // 下拉菜单
     const menu = (
         <Menu>
-            { isLogin ? (
+            {isLogin ? (
                 <Menu.Item style={{ display: 'flex', alignItems: 'center' }}>
                     <ExportOutlined style={{ fontSize: '16px' }} />
-                    <div style={{ fontSize: '16px', padding: '0 12px' }} onClick={async () => {
-                        await logout();
-                        checkUserLogin();
-                    }}>{formatMessage({ id: 'header.signout' })}</div>
+                    <div
+                        style={{ fontSize: '16px', padding: '0 12px' }}
+                        onClick={async () => {
+                            await logout();
+                            checkUserLogin();
+                        }}
+                    >
+                        {formatMessage({ id: 'header.signout' })}
+                    </div>
                 </Menu.Item>
-            ) : null }
+            ) : null}
             <Menu.Item style={{ display: 'flex', alignItems: 'center' }}>
                 <QuestionOutlined style={{ fontSize: '16px' }} />
-                <div style={{ fontSize: '16px', padding: '0 12px' }} onClick={() => {}}>{formatMessage({ id: 'header.feedback' })}</div>
+                <div style={{ fontSize: '16px', padding: '0 12px' }} onClick={() => {}}>
+                    {formatMessage({ id: 'header.feedback' })}
+                </div>
             </Menu.Item>
         </Menu>
     );
@@ -141,19 +163,37 @@ const App: React.FC<{ language: string; getLanguage: Function; deviceList: Devic
                 { title: formatMessage({ id: 'device.card.voltage' }), content: `${voltage}V` },
                 { title: formatMessage({ id: 'device.card.current' }), content: `${current}A` },
             ];
-            return <IW100Card key={key} deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }} channel={{ stat: data.params.switch, name: formatMessage({ id: 'device.card.channel.single' }) }} ballData={ballData} />;
+            return (
+                <IW100Card
+                    key={key}
+                    deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }}
+                    channel={{ stat: data.params.switch, name: formatMessage({ id: 'device.card.channel.single' }) }}
+                    ballData={ballData}
+                />
+            );
         } else if (isPowerDet(uiid)) {
-            return <PowerDetCard key={deviceId} deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }} channel={{ stat: data.params.switch, name: formatMessage({ id: 'device.card.channel.single' }) }} power={`${data.params.power}W`} />;
+            return (
+                <PowerDetCard
+                    key={deviceId}
+                    deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }}
+                    channel={{ stat: data.params.switch, name: formatMessage({ id: 'device.card.channel.single' }) }}
+                    power={`${data.params.power}W`}
+                />
+            );
         } else if (isSocketSwitchDevice(uiid)) {
             const channels: { name: string; stat: 'on' | 'off' }[] = [];
             let len = 0;
-            if (uiid === 1 && type === 'diy') {                         // 单通道 DIY
+            if (uiid === 1 && type === 'diy') {
+                // 单通道 DIY
                 channels.push({ name: formatMessage({ id: 'device.card.channel.single' }), stat: data.params.data1.switch });
-            } else if (uiid === 1 || uiid === 6 || uiid === 14) {       // 单通道非 DIY
+            } else if (uiid === 1 || uiid === 6 || uiid === 14) {
+                // 单通道非 DIY
                 channels.push({ name: formatMessage({ id: 'device.card.channel.single' }), stat: data.params.switch });
-            } else if (uiid === 77 || uiid === 78 || uiid === 112) {    // 单通道（多通道协议）
+            } else if (uiid === 77 || uiid === 78 || uiid === 112) {
+                // 单通道（多通道协议）
                 channels.push({ name: formatMessage({ id: 'device.card.channel.single' }), stat: data.params.switches[0].switch });
-            } else if (uiid === 2 || uiid === 7 || uiid === 113) {      // 双通道
+            } else if (uiid === 2 || uiid === 7 || uiid === 113) {
+                // 双通道
                 len = 2;
             } else if (uiid === 3 || uiid === 8 || uiid === 114) {
                 // 三通道
@@ -163,14 +203,22 @@ const App: React.FC<{ language: string; getLanguage: Function; deviceList: Devic
                 len = 4;
             }
             for (let i = 0; i < len; i++)
-                if (data.tags)
-                    channels.push({ name: data.tags[i], stat: data.params.switches[i].switch });
-                else
-                    channels.push({ name: formatMessage({ id: 'device.card.channel.multi' }, { i: i+1 }), stat: data.params.switches[i].switch });
+                if (data.tags) channels.push({ name: data.tags[i], stat: data.params.switches[i].switch });
+                else channels.push({ name: formatMessage({ id: 'device.card.channel.multi' }, { i: i + 1 }), stat: data.params.switches[i].switch });
 
             return <SocketSwitchCard key={key} deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }} channels={channels} />;
         } else if (isTempDevice(uiid)) {
-            return <TempCard key={deviceId} deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }} channel={{ stat: data.params.switch, name: formatMessage({ id: 'device.card.channel.single' }) }} unit={data.unit} mode={data.params.deviceType} humi={data.params.currentHumidity} temp={data.params.currentTemperature} />;
+            return (
+                <TempCard
+                    key={deviceId}
+                    deviceData={{ fwVersion: data.params.fwVersion, ...deviceData }}
+                    channel={{ stat: data.params.switch, name: formatMessage({ id: 'device.card.channel.single' }) }}
+                    unit={data.unit}
+                    mode={data.params.deviceType}
+                    humi={data.params.currentHumidity}
+                    temp={data.params.currentTemperature}
+                />
+            );
         } else {
             return <UnsupportedCard key={key} deviceData={deviceData} />;
         }
@@ -182,14 +230,23 @@ const App: React.FC<{ language: string; getLanguage: Function; deviceList: Devic
                 <div className={styles['title']}>
                     <h1>eWeLink Smart Home</h1>
                 </div>
-                <div className="control">
-                    { !isLogin ? <Button size="large" shape="round" icon={<UserOutlined />} onClick={() => setLoginTabVisible(true)}>{formatMessage({ id: 'header.signin' })}</Button> : null }
-                    <Button shape="circle" icon={<RedoOutlined />} size="large" onClick={async () => {
-                        const res = await getDeviceList({ type: 'refresh' });
-                        saveDeviceList(res.data);
-                    }} />
+                <div className='control'>
+                    {!isLogin ? (
+                        <Button size='large' shape='round' icon={<UserOutlined />} onClick={() => setLoginTabVisible(true)}>
+                            {formatMessage({ id: 'header.signin' })}
+                        </Button>
+                    ) : null}
+                    <Button
+                        shape='circle'
+                        icon={<RedoOutlined />}
+                        size='large'
+                        onClick={async () => {
+                            const res = await getDeviceList({ type: 'refresh' });
+                            saveDeviceList(res.data);
+                        }}
+                    />
                     <Dropdown overlay={menu}>
-                        <Button shape="circle" icon={<EllipsisOutlined />} size="large" />
+                        <Button shape='circle' icon={<EllipsisOutlined />} size='large' />
                     </Dropdown>
                 </div>
             </header>
@@ -246,10 +303,14 @@ const App: React.FC<{ language: string; getLanguage: Function; deviceList: Devic
                 <p>login: {isLogin ? 'YES' : 'NO'}</p>
             </div>
 
-            <LoginTab visible={loginTabVisible} onClose={() => setLoginTabVisible(false)} onLogin={() => {
-                setLoginTabVisible(false);
-                checkUserLogin();
-            }} />
+            <LoginTab
+                visible={loginTabVisible}
+                onClose={() => setLoginTabVisible(false)}
+                onLogin={() => {
+                    setLoginTabVisible(false);
+                    checkUserLogin();
+                }}
+            />
         </div>
     );
 };
