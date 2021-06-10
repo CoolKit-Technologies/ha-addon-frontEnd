ca25147ff07a02f951eca3acaba7a8eb5f72392a
Tony Yuan <tony.yuan@coolkit.cn>
2021-06-10 10:09:30 +0800

    Add some APIs for device and user

--- src/api/device.ts
+++ src/api/device.ts
@@ -19,3 +19,100 @@ export async function getDeviceListInit() {
 export async function getDeviceListRefresh() {
     return await sendHttpRequest('GET', apiPrefix + '/devices/refresh', { type: 7 });
 }
+
+/**
+ * Set DIY device status
+ */
+export async function setDiyDevice(params: {
+    id: string;         // device id
+    type: string;       // set type: switch
+    params: any;        // control status
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/diy', params);
+}
+
+/**
+ * Set LAN device status
+ */
+export async function setLanDevice(params: {
+    id: string;         // device id
+    apikey: string;     // device apikey
+    params: any;        // control status
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/lan', params);
+}
+
+/**
+ * Set Cloud device status
+ */
+export async function setCloudDevice(params: {
+    id: string;         // device id
+    apikey: string;     // device apikey
+    params: any;        // control status
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/proxy2ws', params);
+}
+
+/**
+ * Remove device in HASS
+ */
+export async function disableDevice(params: {
+    id: string;         // device id
+    disabled: boolean;  // true - disabled
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/disabled', params);
+}
+
+/**
+ * Set temperature unit
+ */
+export async function setTempUnit(params: {
+    id: string;         // device id
+    unit: string;       // temperature unit, 'c' or 'f'
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/device/unit', params);
+}
+
+/**
+ * Set device name
+ */
+export async function setName(params: {
+    id: string;         // device id
+    newName: string;    // new device name
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/updateName', params);
+}
+
+/**
+ * Set device tags
+ */
+export async function setTags(params: {
+    id: string;         // device id
+    tags: any;          // example: { 0: 'name0', 1: 'name1', 2: 'name2', 3: 'name4' }
+}) {
+    return await sendHttpRequest('POST', apiPrefix + 'devices/updateChannelName', params);
+}
+
+/**
+ * Get device OTA information
+ */
+export async function getOtaInfo(params: {
+    list: Array<{
+        deviceid: string;
+        model: string;
+        version: string;
+    }>;
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/getOTAinfo', params);
+}
+
+/**
+ * Upgrade device firmware version
+ */
+export async function upgradeDeviceFw(params: {
+    id: string;             // device id
+    apikey: string;         // device apikey
+    params: any;            // upgrade params
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/devices/device/upgrade', params);
+}
--- src/api/user.ts
+++ src/api/user.ts
@@ -17,3 +17,34 @@ export async function login(params: {
 }) {
     return await sendHttpRequest('POST', apiPrefix + '/user/login', params);
 }
+
+/**
+ * User logout API
+ */
+export async function logout() {
+    return await sendHttpRequest('POST', apiPrefix + '/user/logout');
+}
+
+/**
+ * Determine if user is login
+ */
+export async function isLogin() {
+    return await sendHttpRequest('POST', apiPrefix + '/user/isLogin');
+}
+
+/**
+ * Determine if user is auth
+ */
+export async function isAuth() {
+    return await sendHttpRequest('GET', apiPrefix + '/user/isAuth');
+}
+
+/**
+ * Get Hass token
+ */
+export async function getHaToken(params: {
+    code: string;
+    clientId: string;
+}) {
+    return await sendHttpRequest('POST', apiPrefix + '/user/auth', params);
+}
